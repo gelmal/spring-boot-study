d13bc24090095493ca7e00ea2e483febfda568b2
강상연 <sykang4966@ibizsoftware.net>
2023-08-28 11:39:09 +0900

    change yaml and add Actuator

--- src/main/java/com/example/springbootstudy/SpringBootStudyApplication.java
+++ src/main/java/com/example/springbootstudy/SpringBootStudyApplication.java
@@ -1,8 +1,11 @@
 package com.example.springbootstudy;
 
+import com.example.springbootstudy.dto.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 
@@ -12,6 +15,12 @@ public class SpringBootStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
     }
 
 }
